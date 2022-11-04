204d07be6c2e6065bd1d0cc2924f9416f4b0d9b4
Faiziev <99737952+Faiziev@users.noreply.github.com>
2022-11-04 17:52:10 +0300

    Update hakkimizda.js

--- pages/hakkimizda.js
+++ pages/hakkimizda.js
@@ -18,7 +18,7 @@ export default function Blog() {
         <meta charset="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="keywords" content="Kasap Faiziev, Faiziev kurban, beykoz adak, beykoz kurban, kurban satışı, adak kurban, akika kurban, adak, akika, şükür kurban, kurban kesimi, kurban satışı, kasap-faiziev, faiziev-kasap, kasap, adak faiziev, faiziev adak" />
-        <meta name="description" content="Kasap Faiziev Kurban Satış Evi - Beykoz Anadolu Feneri köyümüzde başlamış olduğumuz hayvancılık alanındaki faaliyetlerimizi 20 yılı aşkın süredir sürdürmekteyiz. 2007 yılından itibaren Beykoz Yüşa'da hizmet vermeye başladık." />
+        <meta name="description" content="Yıldırım Kurban Satış Evi, alanındaki faaliyetlerini 20 yılı aşkın süredir sürdürmektedir. 2007 yılından itibaren Yıldırım Kurban Satış Evi olarak Esenyurt bölgesinde faaliyet göstermektedir." />
         <meta name="author" content="Kasap Faiziev Kurban Satış Evi" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
